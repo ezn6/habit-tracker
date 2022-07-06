71f81f7d7fd5cc372d2363f2fa4037265ad12c85
ezn6 <kcourtney@naver.com>
2022-07-06 09:28:56 +0900

    handleDecrement, handleDelete 함수 생성

--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -11,25 +11,23 @@ class Habits extends Component {
   };
 
   handleIncrement = (habit) => {
-    console.log('plus!');
     const habits = [...this.state.habits];
-    habits.forEach((item) => {
-      if (item.id === habit.id) {
-        item.count++;
-      }
-    });
-    this.setState({ habits: habits });
-    console.log(habits);
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habits });
   };
 
   handleDecrement = (habit) => {
-    console.log(habit);
-    console.log('minus!');
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    habits[index].count = count < 0 ? 0 : count;
+    this.setState({ habits });
   };
 
   handleDelete = (habit) => {
-    console.log(habit);
-    console.log('delete!');
+    const habits = this.state.habits.filter((item) => habit.id !== item.id);
+    this.setState({ habits });
   };
 
   render() {
